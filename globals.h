b50f53b5dbd02b3a4c6976164ea89f798f2dcff8
Wolfram Schneider <wosch@freebsd.org>
2018-01-27 16:40:30 +0000

    apply patch file: patch-globals.h

--- globals.h
+++ globals.h
@@ -13,7 +13,6 @@ Please read the file COPYRIGHT for further details.
 # include <sys/param.h>
 #endif
 
-
 /* globals for socket */
 
 #define IN	0		/* standard input */
@@ -27,9 +26,16 @@ Please read the file COPYRIGHT for further details.
 #define A(args) ()
 #endif
 
+#ifndef USE_INET6
 int create_server_socket A((int port, int queue_length)) ;
 int create_client_socket A((char **hostname, int port)) ;
 int resolve_service A((char *name_or_number, char *protocol, char **name)) ;
+#else /* USE_INET6 */
+int *create_server_sockets A((char **port, int queue_length)) ;
+int create_client_socket A((char **hostname, char **port)) ;
+int socket_local_name A((int socket, char **name, char **ipname, char **port));
+int socket_remote_name A((int socket, char **name, char **ipname, char **port));
+#endif /* USE_INET6 */
 void catchsig A((int sig)) ;
 void usage A((void)) ;
 int do_read_write A((int from, int to)) ;
